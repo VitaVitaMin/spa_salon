b5bebd4cbefba6d62867063acfc280b6be5a76ce
VitaVitaMin <22220podles@gmail.com>
2025-04-30 00:59:15 +0300

    Create services_config.js

---
+++ services_config.js
@@ -0,0 +1,25 @@
+// services-config.js
+export const services = [
+  {
+    id: 1,
+    title: 'Шведский массаж',
+    description: 'Снимает мышечное напряжение и улучшает кровообращение.',
+    img: 'img/service-swedish.jpg',
+    price: 'от 2 000 ₽'
+  },
+  {
+    id: 2,
+    title: 'Ароматерапия',
+    description: 'Массаж с эфирными маслами для полного расслабления.',
+    img: 'img/service-aromatherapy.jpg',
+    price: 'от 2 500 ₽'
+  },
+  {
+    id: 3,
+    title: 'Горячие камни',
+    description: 'Глубокое прогревание мышц горячими базальтовыми камнями.',
+    img: 'img/service-hotstones.jpg',
+    price: 'от 3 000 ₽'
+  }
+  // здесь легко добавлять новые объекты
+];
